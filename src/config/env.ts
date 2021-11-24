5f2eb4c01d4bc972727351a68df85ae188798747
Angel S. Moreno <angelxmoreno@gmail.com>
2021-11-23 20:17:25 -0500

    added type parsing helper functions to env

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,3 +3,9 @@ import dotenvExpand from 'dotenv-expand';
 
 const myEnv = dotenv.config();
 dotenvExpand(myEnv);
+
+const asString = (path: string): string => String(path);
+const asBoolean = (path: string): boolean => path === 'true';
+const asNumber = (path: string): number => Number(path);
+
+export default { asString, asBoolean, asNumber };
