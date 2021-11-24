9a4f106125f1c531c025256c0747fd81ec9e48ba
Angel S. Moreno <angelxmoreno@gmail.com>
2021-11-23 20:17:25 -0500

    lint fixes

--- src/controllers/HelloWorldController.ts
+++ src/controllers/HelloWorldController.ts
@@ -1,17 +1,9 @@
-import { Controller, Get, QueryParam, Req, Res } from 'routing-controllers';
-import { Request, Response } from 'express';
+import { Controller, Get } from 'routing-controllers';
 
 @Controller('')
 export class HelloWorldController {
     @Get('/hello-world')
-    index(@Req() req: Request, @Res() res: Response) {
+    index() {
         return 'hello world!';
-    }
-
-    @Get('/')
-    test(@Req() request: Request, @Res() response: Response) {
-        return {
-            foo: JSON.stringify(request.toString()),
-        };
     }
 }
